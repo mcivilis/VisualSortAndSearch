e6053d82107e62be153b421c5ab38a0c5bf70b82
Maria Civilis <maria.civilis@gmail.com>
2021-06-19 12:37:05 -0400

    Initial Commit

---
+++ VisualSortAndSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisualSortAndSearch
+//
+//  Created by Maria Civilis on 2021-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisualSortAndSearch/VisualSortAndSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualSortAndSearchApp.swift
+//  VisualSortAndSearch
+//
+//  Created by Maria Civilis on 2021-06-19.
+//
+
+import SwiftUI
+
+@main
+struct VisualSortAndSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
