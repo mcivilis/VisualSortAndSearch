49c0b68944d3964b3fbbf9f4db0cc701f4732659
Maria Civilis <maria.civilis@gmail.com>
2021-06-19 14:27:35 -0400

    Implement bubble sort

--- VisualSortAndSearch/Algorithms/Sort.swift
+++ VisualSortAndSearch/Algorithms/Sort.swift
@@ -34,7 +34,21 @@ func sort(_ values: [Int], using sortType: SortType) -> [Int] {
 // MARK: - Private
 
 fileprivate func bubbleSort(_ values: [Int]) -> [Int] {
-    return values.sorted()
+    var sorted = values
+    for k in 0..<values.count {
+        // Each pass should end when we reach second last value
+        // At each pass we have one less value to check
+        let indicesToCheck = values.count - k - 1
+        for i in 0..<indicesToCheck{
+            if sorted[i] > sorted[i+1] {
+                // Swap
+                let tmp = sorted[i]
+                sorted[i] = sorted[i + 1]
+                sorted[i + 1] = tmp
+            }
+        }
+    }
+    return sorted
 }
 
 fileprivate func selectionSort(_ values: [Int]) -> [Int] {
